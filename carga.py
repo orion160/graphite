9909b60f1247fdf64d89e02206850d6a349164b1
o.urregov <o.urregov@uniandes.edu.co>
2023-11-05 23:54:34 -0500

    con visualización

--- carga.py
+++ carga.py
@@ -1,5 +1,39 @@
+
+# First networkx library is imported  
+# along with matplotlib 
+import networkx as nx 
+import matplotlib.pyplot as plt 
 import sys
 
+# Defining a Class 
+class GraphVisualization: 
+   
+    def __init__(self, caso): 
+          
+        # visual is a list which stores all  
+        # the set of edges that constitutes a 
+        # graph 
+        self.visual = [] 
+        self.caso = caso
+          
+    # addEdge function inputs the vertices of an 
+    # edge and appends it to the visual list 
+    def addEdge(self, a, b):
+        temp = [a, b] 
+        self.visual.append(temp) 
+          
+    # In visualize function G is an object of 
+    # class Graph given by networkx G.add_edges_from(visual) 
+    # creates a graph with a given list 
+    # nx.draw_networkx(G) - plots the graph 
+    # plt.show() - displays the graph 
+    def visualize(self): 
+        G = nx.DiGraph() 
+        G.add_edges_from(self.visual)
+        plt.figure(self.caso) 
+        nx.draw_networkx(G, arrows=True)
+        plt.savefig('graph'+str(self.caso)+'.png')
+        #plt.show() 
 
 def main():
     
@@ -15,6 +49,9 @@ def main():
         N = int(datos[1])
         k = int(datos[2])
 
+        # la matriz
+
+        matriz = [[0 for _ in range(N)] for _ in range(M)]
         #diccionario con las coordenadas del mínimo rectángulo de cada llave. Cada valor de cada llave es una lista con [min_x,min_y,max_x,max_y]
         
         dic = {}
@@ -29,6 +66,9 @@ def main():
             for j in range(N):
                 
                 llave = int(llaves[j])
+                
+                # vamos llenamos la matriz
+                matriz[i][j] = llave
                 
                 # Si la llave es nueva, la agregamos al diccionario
 
@@ -67,5 +107,24 @@ def main():
             print("max_x = " + str(dic[llavei][2]))
             print("max_y = " + str(dic[llavei][3]))
             print("-----------------\n") """
+        
+        G = GraphVisualization(caso) 
+        
+        for llave in range(1,k+1):
+            
+            hijos = set()
+            for row in range(dic[llave][1],dic[llave][3]+1):
+                for col in range(dic[llave][0],dic[llave][2]+1):
+                    hijos.add(matriz[row][col])
+            
+            hijos.remove(llave)
+            
+            for hijo in hijos:
+                G.addEdge(llave, hijo) 
+                
+        G.visualize() 
+
+
+
                 
 main()
